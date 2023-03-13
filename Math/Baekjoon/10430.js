554788bed67b27b5e017012b07d5f40b2d840c41
wish0ne <wish@khu.ac.kr>
2023-03-13 23:06:09 +0900

    [백준][Math] 10430 나머지 (브5) 모듈러 연산의 대표적 속성 두가지 1. (A+B) % C = (A%C + B%C) % C 2. (A*B) % C = (A%C * B%C) % C

---
+++ Math/Baekjoon/10430.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const file = "./input.txt";
+// const file = '/dev/stdin';
+const input = fs.readFileSync(file).toString().trim().split("\n");
+
+const [a, b, c] = input[0].split(" ").map(Number);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
+
+//solve
